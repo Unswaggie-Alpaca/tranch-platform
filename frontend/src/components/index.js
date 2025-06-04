54f30f98e26d5738625fcaf7d604c24ee8df68e2
Luc <luc031000@gmail.com>
2025-06-05 00:28:46 +1000

    Revert "refactor: extract dashboard and common components"
    This reverts commit 752a35735183ba6bc7fb87c4bdb3a5ec70e62f58.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,12 +2,7 @@ export * from './DocumentManager';
 export * from './Modal';
 export * from './NumberInput';
 export * from './ProtectedRoute';
-export * from './common';
-export { default as PaymentModal } from './PaymentModal';
-export { default as SubscriptionModal } from './SubscriptionModal';
-export { default as ProjectCard } from './ProjectCard';
 
 export * from "./DocumentRequestModal";
 export * from "./FileIcon";
-export { default as Navigation } from './Navigation';
-export { default as BrokerAIFloating } from './BrokerAIFloating';
+export { default as Navbar } from "./Navbar";
